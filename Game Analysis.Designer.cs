98e04b5b38a60e2e72945834504e5e10c1637d59
retreat1188 <67943757+retreat1188@users.noreply.github.com>
2021-11-08 14:59:19 -0800

    Add files via upload

---
+++ Game Analysis.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace CSharp___Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLap1 = new System.Windows.Forms.TextBox();
+            this.txtLap2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLap3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLap4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLap5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblMinLapTime = new System.Windows.Forms.Label();
+            this.lblMaxLapTime = new System.Windows.Forms.Label();
+            this.lblAvgLapTime = new System.Windows.Forms.Label();
+            this.lblSumofLapTimes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(25, 64);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(424, 52);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lap 1";
+            // 
+            // txtLap1
+            // 
+            this.txtLap1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLap1.Location = new System.Drawing.Point(25, 32);
+            this.txtLap1.Name = "txtLap1";
+            this.txtLap1.Size = new System.Drawing.Size(70, 26);
+            this.txtLap1.TabIndex = 2;
+            // 
+            // txtLap2
+            // 
+            this.txtLap2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLap2.Location = new System.Drawing.Point(113, 32);
+            this.txtLap2.Name = "txtLap2";
+            this.txtLap2.Size = new System.Drawing.Size(70, 26);
+            this.txtLap2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lap 2";
+            // 
+            // txtLap3
+            // 
+            this.txtLap3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLap3.Location = new System.Drawing.Point(205, 32);
+            this.txtLap3.Name = "txtLap3";
+            this.txtLap3.Size = new System.Drawing.Size(70, 26);
+            this.txtLap3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(213, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lap 3";
+            // 
+            // txtLap4
+            // 
+            this.txtLap4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLap4.Location = new System.Drawing.Point(292, 32);
+            this.txtLap4.Name = "txtLap4";
+            this.txtLap4.Size = new System.Drawing.Size(70, 26);
+            this.txtLap4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(300, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lap 4";
+            // 
+            // txtLap5
+            // 
+            this.txtLap5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLap5.Location = new System.Drawing.Point(379, 32);
+            this.txtLap5.Name = "txtLap5";
+            this.txtLap5.Size = new System.Drawing.Size(70, 26);
+            this.txtLap5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(387, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Lap 5";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 30);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Minimum Lap Time:";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(199, 30);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Maximum Lap Time:";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(21, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(199, 30);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Average Lap Time:";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(21, 209);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(199, 30);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Sum of Lap Times:";
+            // 
+            // lblMinLapTime
+            // 
+            this.lblMinLapTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMinLapTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinLapTime.Location = new System.Drawing.Point(217, 121);
+            this.lblMinLapTime.Name = "lblMinLapTime";
+            this.lblMinLapTime.Size = new System.Drawing.Size(232, 28);
+            this.lblMinLapTime.TabIndex = 15;
+            // 
+            // lblMaxLapTime
+            // 
+            this.lblMaxLapTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMaxLapTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxLapTime.Location = new System.Drawing.Point(217, 149);
+            this.lblMaxLapTime.Name = "lblMaxLapTime";
+            this.lblMaxLapTime.Size = new System.Drawing.Size(232, 28);
+            this.lblMaxLapTime.TabIndex = 16;
+            // 
+            // lblAvgLapTime
+            // 
+            this.lblAvgLapTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAvgLapTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvgLapTime.Location = new System.Drawing.Point(217, 177);
+            this.lblAvgLapTime.Name = "lblAvgLapTime";
+            this.lblAvgLapTime.Size = new System.Drawing.Size(232, 28);
+            this.lblAvgLapTime.TabIndex = 17;
+            // 
+            // lblSumofLapTimes
+            // 
+            this.lblSumofLapTimes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSumofLapTimes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSumofLapTimes.Location = new System.Drawing.Point(217, 205);
+            this.lblSumofLapTimes.Name = "lblSumofLapTimes";
+            this.lblSumofLapTimes.Size = new System.Drawing.Size(232, 28);
+            this.lblSumofLapTimes.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 266);
+            this.Controls.Add(this.lblSumofLapTimes);
+            this.Controls.Add(this.lblAvgLapTime);
+            this.Controls.Add(this.lblMaxLapTime);
+            this.Controls.Add(this.lblMinLapTime);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtLap5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLap4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLap3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLap2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLap1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Race Analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLap1;
+        private System.Windows.Forms.TextBox txtLap2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLap3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLap4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLap5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblMinLapTime;
+        private System.Windows.Forms.Label lblMaxLapTime;
+        private System.Windows.Forms.Label lblAvgLapTime;
+        private System.Windows.Forms.Label lblSumofLapTimes;
+    }
+}
+
---
+++ Game Analysis.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp___Lab12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //constant
+        const int TOTALLAPS = 5;
+
+        //global (i.e., modular) variables
+        string minLapTime = "";
+        string maxLapTime = "";
+        string avgLapTime = "";
+        string sumOfLapTimes = "";
+
+        int minLapTimeInSeconds;
+        int maxLapTimeInSeconds;
+        int avgLapTimeInSeconds;
+        int sumOfLapTimesInSeconds;
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //declarations
+            int lap1Time;
+            int lap2Time;
+            int lap3Time;
+            int lap4Time;
+            int lap5Time;
+
+            //get the user input
+            lap1Time = Convert.ToInt32(txtLap1.Text);
+            lap2Time = Convert.ToInt32(txtLap2.Text);
+            lap3Time = Convert.ToInt32(txtLap3.Text);
+            lap4Time = Convert.ToInt32(txtLap4.Text);
+            lap5Time = Convert.ToInt32(txtLap5.Text);
+
+            //calculations
+            calcMinLapTime(lap1Time, lap2Time, lap3Time, lap4Time, lap5Time);
+            calcMaxLapTime(lap1Time, lap2Time, lap3Time, lap4Time, lap5Time);
+            calcAvgLapTime(lap1Time, lap2Time, lap3Time, lap4Time, lap5Time);
+            calcSumOfLapTimes(lap1Time, lap2Time, lap3Time, lap4Time, lap5Time);
+
+            //output
+            lblMinLapTime.Text = minLapTime;
+            lblMaxLapTime.Text = maxLapTime;
+            lblAvgLapTime.Text = avgLapTime;
+            lblSumofLapTimes.Text = sumOfLapTimes;
+        }
+
+        void calcMinLapTime(int lap1TimePar, int lap2TimePar, int lap3TimePar,
+            int lap4TimePar, int lap5TimePar)
+        {
+            int minimumTime;
+            int minutes;
+            int seconds;
+
+            minimumTime = Math.Min(lap1TimePar, Math.Min(lap2TimePar, Math.Min(
+                lap3TimePar, Math.Min(lap4TimePar, lap5TimePar))));
+
+            if (minimumTime >= 60) 
+            {
+                minutes = (int) (minimumTime / 60);
+                seconds = minimumTime - minutes * 60;
+                minLapTime = minutes.ToString() + "\'" + seconds.ToString() + "\"";
+            }
+            else
+            {
+                minutes = 0;
+                seconds = minimumTime;
+                minLapTime = seconds.ToString() + "\"";
+            }
+        }
+
+        void calcMaxLapTime(int lap1TimePar, int lap2TimePar, int lap3TimePar,
+            int lap4TimePar, int lap5TimePar)
+        {
+            int maximumTime;
+            int minutes;
+            int seconds;
+
+            maximumTime = Math.Max(lap1TimePar, Math.Max(lap2TimePar, Math.Max(
+                lap3TimePar, Math.Max(lap4TimePar, lap5TimePar))));
+
+            if (maximumTime >= 60)
+            {
+                minutes = (int)(maximumTime / 60);
+                seconds = maximumTime - minutes * 60;
+                maxLapTime = minutes.ToString() + "\'" + seconds.ToString() + "\"";
+            }
+            else
+            {
+                minutes = 0;
+                seconds = maximumTime;
+                maxLapTime = seconds.ToString() + "\"";
+            }
+        }
+
+        void  calcAvgLapTime(int lap1TimePar, int lap2TimePar, int lap3TimePar,
+            int lap4TimePar, int lap5TimePar)
+        {
+            int minutes;
+            int seconds;
+
+            avgLapTimeInSeconds = (lap1TimePar + lap2TimePar + lap3TimePar +
+                lap4TimePar + lap5TimePar) / TOTALLAPS; //83 -> 1'23"
+            if (avgLapTimeInSeconds >= 60)
+            {
+                minutes = (int)(avgLapTimeInSeconds / 60);
+                seconds = avgLapTimeInSeconds - minutes * 60;
+                avgLapTime = minutes.ToString() + "\'" + seconds.ToString() + "\"";
+            }
+            else
+            {
+                minutes = 0;
+                seconds = avgLapTimeInSeconds;
+                avgLapTime = seconds.ToString() + "\""; //53"
+            }
+        }
+
+        void calcSumOfLapTimes(int lap1TimePar, int lap2TimePar, int lap3TimePar,
+            int lap4TimePar, int lap5TimePar)
+        {
+            int minutes;
+            int seconds;
+
+            sumOfLapTimesInSeconds = lap1TimePar + lap2TimePar + lap3TimePar +
+                lap4TimePar + lap5TimePar;
+
+            if (sumOfLapTimesInSeconds >= 60)
+            {
+                minutes = (int)(sumOfLapTimesInSeconds / 60);
+                seconds = sumOfLapTimesInSeconds - minutes * 60;
+                sumOfLapTimes = minutes.ToString() + "\'" + seconds.ToString() + "\"";
+            }
+            else
+            {
+                minutes = 0;
+                seconds = sumOfLapTimesInSeconds;
+                sumOfLapTimes = seconds.ToString() + "\"";
+            }
+        }
+    }
+}
